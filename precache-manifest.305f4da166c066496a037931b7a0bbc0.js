74e05839b3150113c6b8c318b16b8e3e0fd96617
mario <contato@magoro.com.br>
2021-01-20 22:56:46 -0300

    first commit

---
+++ precache-manifest.305f4da166c066496a037931b7a0bbc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2448b00dce260460df5cab7f8c9388dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28cf466b66d81d700fe2",
+    "url": "/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "18e213c5727182e82c6b",
+    "url": "/static/css/main.8f6fa163.chunk.css"
+  },
+  {
+    "revision": "28cf466b66d81d700fe2",
+    "url": "/static/js/2.fef27ff1.chunk.js"
+  },
+  {
+    "revision": "b952d6d32f30bab0684e61579c5d99b7",
+    "url": "/static/js/2.fef27ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18e213c5727182e82c6b",
+    "url": "/static/js/main.7ac966f7.chunk.js"
+  },
+  {
+    "revision": "969ec6062bb99b0b9815",
+    "url": "/static/js/runtime-main.a54c6ea9.js"
+  }
+]);
